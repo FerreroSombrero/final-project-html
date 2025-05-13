df5137d351cf4f50564af38871c9b9aba3fdde42
Adam <adamhrdlicka2012@post.cz>
2025-05-13 19:32:20 +0200

    yes

---
+++ script.js
@@ -0,0 +1,19 @@
+function move() {
+    gsap.to(".box", {
+        x: 100,
+        duration: 2
+    })
+}
+
+    gsap.from(".box", {
+        opacity: 0,
+        rotate: 100,
+        duration: 5
+    })
+
+gsap.registerPlugin(SplitText)
+
+let split - SplitText.create('#heading', {
+    
+})
+
